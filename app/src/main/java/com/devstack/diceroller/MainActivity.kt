a21fac1c937874ed95740b281c66e493d5a537cb
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-04-26 12:52:29 +0530

    dice image xml setup

--- app/src/main/java/com/devstack/diceroller/MainActivity.kt
+++ app/src/main/java/com/devstack/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devstack.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
@@ -19,9 +20,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when(Random().nextInt(6) + 1) {
+
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_img)
+        diceImage.setImageResource(drawableResource)
 
     }
 }
